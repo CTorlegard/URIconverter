6a284711d7c3a1c393c307771aed6d97534633ff
ctorlegard <christiantorlegard@alohassandals.com>
2023-05-19 14:17:39 +0200

    won't convert duplicates

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ df = pd.read_csv('results.csv')
 # Get the name of the first column in the DataFrame
 first_column = df.columns[0]
 
+# Create a set to keep track of the image URIs that have already been processed
+processed_images = set()
+
 # Loop through each row in the DataFrame
 for index, row in df.iterrows():
     # Get the image URI from the row
@@ -16,18 +19,23 @@ for index, row in df.iterrows():
     
     # Check if the image URI is not "null" or "nan"
     if pd.notnull(image_uri) and image_uri != "null":
-        # Use the requests library to get the image data from the URI
-        response = requests.get(image_uri)
-        img = Image.open(BytesIO(response.content))
-        
-        # Get the value of the first column for this row
-        first_column_value = row[first_column]
-        
-        # Convert the image to JPG and save it to a file with the same name as the first column value
-        img.convert('RGB').save(f'G:\My Drive/bq-dump-converted-images/{first_column_value}.jpg')
-        
-        # Update the row with the path to the saved image file
-        df.at[index, 'image3'] = f'G:\My Drive/bq-dump-converted-images/{first_column_value}.jpg'
+        # Check if the image URI has not already been processed
+        if image_uri not in processed_images:
+            # Use the requests library to get the image data from the URI
+            response = requests.get(image_uri)
+            img = Image.open(BytesIO(response.content))
+            
+            # Get the value of the first column for this row
+            first_column_value = row[first_column]
+            
+            # Convert the image to JPG and save it to a file with the same name as the first column value
+            img.convert('RGB').save(f'G:\My Drive/bq-dump-converted-images/{first_column_value}.jpg')
+            
+            # Update the row with the path to the saved image file
+            df.at[index, 'image3'] = f'G:\My Drive/bq-dump-converted-images/{first_column_value}.jpg'
+            
+            # Add the image URI to the set of processed images
+            processed_images.add(image_uri)
 
 # Save the updated DataFrame back to a CSV file
 df.to_csv('G:\My Drive/bq-dump-converted-csv/bq-dump-converted.csv', index=False)
