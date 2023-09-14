1d76a99f7fe4e2483534825cdfcf4c692ca9d111
ctorlegard <christiantorlegard@alohassandals.com>
2023-09-14 10:29:28 +0200

    refactored to CF

--- main.py
+++ main.py
@@ -1,48 +1,68 @@
-import pandas as pd
+from google.cloud import bigquery
+from google.cloud import storage
 from PIL import Image, UnidentifiedImageError
+import pandas as pd
 import requests
 from io import BytesIO
 
+# Constants
+BUCKET_NAME = 'shopify-converted-images'
 
-# Load the CSV file into a DataFrame. (Change this path so it's relative to your local path. I usually replace the results.csv with the new one I extracted from BQ.)
-# It's also possible to change the path to your google drive, but then it needs to be through the Google Drive Client.
-df = pd.read_csv('results.csv')
+def bq_to_gcs(event, context):
+    # Authenticate with the service account using the Cloud Function's default service account
+    bq_client = bigquery.Client()
+    storage_client = storage.Client()
 
-# Get the name of the first column in the DataFrame
-first_column = df.columns[0]
+    # BigQuery query to fetch data
+    query = """
+    SELECT
+        FORMAT(Product_Sku) AS sku,
+        Product_title AS title,
+        Product_Barcode AS barcode,
+        Product_Variant_Title AS size,
+        SPLIT(images, ',')[SAFE_ORDINAL(3)] AS image3
+    FROM `alohas-shopify.ialohas_shopify.PRODUCTS_COREVIEW`
+    LIMIT 500000
+    OFFSET 178
+    """
+    df = bq_client.query(query).to_dataframe()
 
-# Create a set to keep track of the image URIs that have already been processed
-processed_images = set()
+    # Create a set to keep track of the image URIs that have already been processed
+    processed_images = set()
 
-# Loop through each row in the DataFrame
-for index, row in df.iterrows():
-    # Get the image URI from the row
-    image_uri = row['image3']
-    
-    # Check if the image URI is not "null" or "nan"
-    if pd.notnull(image_uri) and image_uri != "null":
-        # Check if the image URI has not already been processed
-        if image_uri not in processed_images:
-            # Use the requests library to get the image data from the URI
-            response = requests.get(image_uri)
-            try:
-                img = Image.open(BytesIO(response.content))
-            except UnidentifiedImageError:
-                print(f"Unable to open image: {image_uri}")
-                continue
-            
-            # Get the value of the first column for this row
-            first_column_value = row[first_column]
-            
-            # Convert the image to JPG and save it to a file with the same name as the first column value
-            # Might need to change the path here if you are using Linux or Mac.
-            img.convert('RGB').save(f'G:/Shared drives/Shopify-images/{first_column_value}.jpg')
-            
-            # Update the row with the path to the saved image file
-            df.at[index, 'image3'] = f'G:/Shared drives/Shopify-images/{first_column_value}.jpg'
-            
-            # Add the image URI to the set of processed images
-            processed_images.add(image_uri)
+    # Create a Cloud Storage bucket object
+    bucket = storage_client.bucket(BUCKET_NAME)
 
-# Save the updated DataFrame back to a CSV file
-df.to_csv('G:/Shared drives/Shopify-images/converted csv/bq-dump-converted_19_06.csv', index=False)
+    # Loop through each row in the DataFrame
+    for index, row in df.iterrows():
+        # Get the image URI from the row
+        image_uri = row['image3']
+
+        # Check if the image URI is not "null" or "nan"
+        if pd.notnull(image_uri) and image_uri != "null":
+            # Check if the image URI has not already been processed
+            if image_uri not in processed_images:
+                # Use the requests library to get the image data from the URI
+                response = requests.get(image_uri)
+                try:
+                    img = Image.open(BytesIO(response.content))
+                except UnidentifiedImageError:
+                    print(f"Unable to open image: {image_uri}")
+                    continue
+
+                # Get the value of the first column for this row
+                first_column_value = row['sku']
+
+                # Convert the image to JPG and upload it to Cloud Storage
+                img_bytes_io = BytesIO()
+                img.convert('RGB').save(img_bytes_io, format='JPEG')
+                img_bytes_io.seek(0)
+                blob = bucket.blob(f'converted-images/{first_column_value}.jpg')
+                blob.upload_from_file(img_bytes_io, content_type='image/jpeg')
+
+                # Add the image URI to the set of processed images
+                processed_images.add(image_uri)
+
+    print("Images uploaded to Google Cloud Storage")
+
+# Deploying function - Deployment details will be handled using the 'gcloud' CLI
